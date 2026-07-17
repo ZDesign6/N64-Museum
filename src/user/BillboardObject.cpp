0497209549c66b2423aa547e8d7e521c5919dfb6
ZDesign6 <zd7business@gmail.com>
2026-07-17 14:24:13 -0400

    A Torture Device For The Modern Age

---
+++ src/user/BillboardObject.cpp
@@ -0,0 +1,86 @@
+#include "script/userScript.h"
+#include "scene/sceneManager.h"
+#include "debug/debugMenu.h"
+#include "debug/debugDraw.h"
+namespace P64::Script::CA40988B0B39ED89
+{
+  P64_DATA(
+
+      // Put your arguments and runtime values bound to an object here.
+      // If you need them to show up in the editor, add a [[P64::Name("...")]] attribute.
+      //
+      // Types that can be set in the editor:
+      // - uint8_t, int8_t, uint16_t, int16_t, uint32_t, int32_t
+      // - float
+      // - AssetRef<sprite_t>
+      // - ObjectRef
+      //
+      // For unsigned integers (uint8_t/uint16_t/uint32_t) you can add a
+      // [[P64::Bitmask("0=Fire, 1=Water, 2=Earth")]] attribute to edit them as a
+      // named multi-select of bits instead of a plain number.
+      //
+      // Other types can be used but are not exposed in the editor.
+
+      // - EDITOR EXPOSED VARS -
+      [[P64::Name("Player Obj")]]
+      ObjectRef playerObj;
+      // - SCRIPT VARS -
+      fm_quat_t playerRot;
+
+  );
+
+  // The following functions are called by the engine at different points in the object's lifecycle.
+  // If you don't need a specific function you can remove it.
+
+  void init(Object& obj, Data *data)
+  {
+    // initialization, this is called once when the object spawns
+  }
+
+  void destroy(Object& obj, Data *data)
+  {
+    // clean-up, this is called when the object gets deleted
+  }
+
+  void update(Object& obj, Data *data, float deltaTime)
+  {
+    // this is called once every frame, put your main logic here
+
+  }
+
+  void fixedUpdate(Object& obj, Data *data, float fixedDeltaTime)
+  {
+    // this is called on the fixed physics timestep before collision/physics are stepped
+  }
+
+  void draw(Object& obj, Data *data, float deltaTime)
+  {
+      // this is called once every frame, and for every active camera.
+      // Put your drawing code here
+
+      //update the Player's Rot by getting the Player Obj off the Player ObjRef off of data, then deref its rot
+      data->playerRot = data->playerObj.get()->rot;
+      Debug::print(100, 100, "test");
+  }
+
+  void onEvent(Object& obj, Data *data, const ObjectEvent &event)
+  {
+    // generic events an object can receive
+    switch(event.type)
+    {
+      case EVENT_TYPE_READY: // object is fully initialized, no update call has happened yet
+      break;
+      case EVENT_TYPE_ENABLE: // object got enabled
+      break;
+      case EVENT_TYPE_DISABLE: // object got disabled
+      break;
+
+      // you can check for your own custom types here too
+    }
+  }
+
+  void onCollision(Object& obj, Data *data, const Coll::CollEvent& event)
+  {
+    // collision callbacks, only used if any collider is attached
+  }
+}
