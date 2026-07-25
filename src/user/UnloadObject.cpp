01a9a1865c06fe48ba1699cab6de1094c96e018a
ZDesign6 <zd7business@gmail.com>
2026-07-24 23:41:17 -0400

    Work on Unloading Script Complete, must be tested
    Needs obj's to have arbitrary position set in engine to test

--- src/user/UnloadObject.cpp
+++ src/user/UnloadObject.cpp
@@ -26,11 +26,17 @@ namespace P64::Script::C43621A7AFC2FADF
     [[P64::Name("Reference to Player Obj")]]
     ObjectRef playerRef;
     [[P64::Name("Max Viewable X Distance")]]
-    float maxDifferenceX;
+    float maxDifferenceX = 2000.0f;
     [[P64::Name("Max Viewable Y Distance")]]
-    float maxDifferenceY;
+    float maxDifferenceY = 2000.0f;
     [[P64::Name("Max Viewable Z Distance")]]
-    float maxDifferenceZ;
+    float maxDifferenceZ = 2000.0f;
+    [[P64::Name("This Obj's X Pos")]]
+    float objXPos = 0.0f;
+    [[P64::Name("This Obj's Y Pos")]]
+    float objYPos = 0.0f;
+    [[P64::Name("This Obj's Z Pos")]]
+    float objZPos = 0.0f;
 
     // -- SCRIPT GLOBAL VARS --
   );
@@ -56,40 +62,76 @@ namespace P64::Script::C43621A7AFC2FADF
   void fixedUpdate(Object& obj, Data *data, float fixedDeltaTime)
   {
     // this is called on the fixed physics timestep before collision/physics are stepped
+       //tracks if we are within the render distance. Assumed false, set true by logic. Controls visibility.
+      bool withinRenderDistance = true;
+      //re-calculate the difference in Z pos every frame, to avoid denormal errors
+      float xDifference = objXPos - data->playerRef.get()->pos.x;
+      float yDifference = objYPos - data->playerRef.get()->pos.y;
+      float zDifference = objZPos - data->playerRef.get()->pos.z;
+      // this is called once every frame, and for every active camera.
+      // Put your drawing code here
+      Debug::printStart();
+      //printf(x, y, string w/ formatting argument, arg to format)
+      Debug::printf(50, 50, "X Distance from the player: %f", xDifference);
+      Debug::printf(50, 75, "Y Distance from the player: %f", yDifference);
+      Debug::printf(50, 100, "Z Distance from player: %f", zDifference);
+
+      //If object is within maxDifferenceX
+      if (xDifference >= data->maxDifferenceX || xDifference <= (data->maxDifferenceX * -1))
+      {
+          Debug::print(50, 150, "Inside X Distance!");
+          //set obj as inside of Render distance
+          withinRenderDistance = false;
+
+      }
+      //If objeect is outside maxDifferenceX
+      else
+      {
+          Debug::print(50, 150, "Exceeded X Distance!");
+      }
+      //If object is within maxDifferenceY
+      if (yDifference >= data->maxDifferenceY || yDifference <= (data->maxDifferenceY * -1))
+      {
+          Debug::print(50, 175, "Within Y Distance!");
+          //flag as inside render distance
+          withinRenderDistance = false;
+      }
+      //If objeect is outside maxDifferenceY
+      else
+      {
+          Debug::print(50, 175, "Exceeded Y Distance!");
+      }
+      //If object is within maxDifferenceZ
+      if (zDifference >= data->maxDifferenceZ || zDifference <= (data->maxDifferenceZ * -1))
+      {
+          Debug::print(50, 200, "Within Z Distance!");
+          //flag as inside render distance
+          withinRenderDistance = false;
+      }
+      //If object is outside maxDifferenceZ
+      else
+      {
+          Debug::print(50, 200, "Exceeded Z Distance!");
+      }
+      //RENDERING
+
+      //if player is within any of the axes' render distances, render the object
+      if (withinRenderDistance == true)
+      {
+          //set obj to visible
+          obj.setVisible(true);
+      }
+      //if player is outside of all render axes, do not render the object
+      else
+      {
+          //set obj to not visible this frame
+          obj.setVisible(false);
+      }
   }
 
   void draw(Object& obj, Data *data, float deltaTime)
   {
-      //re-calculate the difference in Z pos every frame, to avoid denormal errors
-      float xDifference = obj.pos.x - data->playerRef.get()->pos.x;
-      float yDifference = obj.pos.y - data->playerRef.get()->pos.y;
-      float zDifference = obj.pos.z - data->playerRef.get()->pos.z;
-    // this is called once every frame, and for every active camera.
-    // Put your drawing code here
-      Debug::printStart();
-      //printf(x, y, string w/ formatting argument, arg to format)
-      Debug::printf(50,50,"X Distance from the player: %f", xDifference);
-      Debug::printf(50, 75, "Y Distance from the player: %f", yDifference);
-      //Debug::printf(50, 100, "Z Distance from player: %f", obj.pos.z - data->playerRef.get()->pos.z);
-      Debug::printf(50, 100, "Z Distance from player: %f", zDifference);
-
-      //If object is beyond maxDifferenceX
-      if (xDifference >= data->maxDifferenceX || xDifference <= (data->maxDifferenceX * -1))
-      {
-          Debug::print(50, 150, "Exceeded X Distance!");
-
-      }
-      //If object is beyond maxDifferenceY
-      if (yDifference >= data->maxDifferenceY || yDifference <= (data->maxDifferenceY * -1))
-      {
-          Debug::print(50, 175, "Exceeded Y Distance!");
-      }
-      //If object is beyond maxDifferenceZ
-      if (zDifference >= data->maxDifferenceZ || zDifference <= (data->maxDifferenceZ * -1))
-      {
-          Debug::print(50, 200, "Exceeded Z Distance!");
-      }
-
+     
   }
 
   void onEvent(Object& obj, Data *data, const ObjectEvent &event)
